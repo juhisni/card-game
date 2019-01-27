3bffed7c3720f5b21f4f7e369fd406812b483dde
Juhis <juhis.ni@gmail.com>
2019-01-27 17:13:28 +0200

    Add function for starting a new game

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var cards = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12", "c13", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12", "h13", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13"];
+//var cards = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12", "c13", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12", "h13", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13"];
+var cards;
 var remainingCards;
 //JavaScript implementation of the Fisher-Yates shuffle
 //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -18,4 +19,11 @@ function pickCard(){
   //Stores the location of the picked card's image and then sets that image as the one showing.
   var picked_card_image_location = "images/" + picked_card + ".png";
   document.querySelector(".cardSlot img").setAttribute("src", picked_card_image_location);
+}
+function startNewGame(){
+  document.querySelector(".cardSlot img").setAttribute("src", "images/back.png");
+  document.querySelector(".cardSlot").classList.add("active");
+  cards = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12", "c13", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12", "h13", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13"];
+  shuffleArray(cards);
+  console.log(cards);
 }
