82ea217a84605e5026e1a6d249e6d05b2637abcf
Juhis <juhis.ni@gmail.com>
2019-01-27 16:09:52 +0200

    Add basic function to pick a card from the shuffled deck

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cards = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12", "c13", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "d11", "d12", "d13", "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12", "h13", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11", "s12", "s13"];
+var remainingCards;
 //JavaScript implementation of the Fisher-Yates shuffle
 //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffleArray(cards) {
@@ -6,5 +7,15 @@ function shuffleArray(cards) {
         const j = Math.floor(Math.random() * (i + 1));
         [cards[i], cards[j]] = [cards[j], cards[i]];
     }
-    console.log(cards);
+    //console.log(cards);
+    //Number of cards at the beginning = 52.
+    remainingCards = cards.length;
+    //console.log(remainingCards);
+}
+//Picking the last item of the shuffled array and then removing it from the array
+function pickCard(){
+  var picked_card = cards[remainingCards - 1];
+  cards.pop();
+  //console.log(picked_card);
+  //console.log(cards);
 }
